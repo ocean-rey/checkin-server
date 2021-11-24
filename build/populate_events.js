00b7520464d6ebc269b7a3ff2ecfc53088d620b7
ocean-rey <x7flux@gmail.com>
2021-11-25 01:38:55 +0300

    removed files according to gitignore

--- build/populate_events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_fetch_1 = __importDefault(require("node-fetch"));
-(0, node_fetch_1.default)("https://www.visitsaudi.com/bin/api/v1/events?locale=en").then(res => res.json().then(events => {
-    console.log(events);
-}));
